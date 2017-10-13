e3f16455189859d0a97c3e6bc65176527a415b0f
Justin Ivany <1406666+jrivany@users.noreply.github.com>
2017-10-13 09:49:23 -0600

    Added readme and example

---
+++ README.md
@@ -0,0 +1,16 @@
+# ResElmE
+
+A little project I wrote for fun while learning Elm and updating my resume.
+
+Inteded to be used with
+[elm-static-html]: https://www.npmjs.com/package/elm-static-html
+Personally I use pandoc and context to generate a PDF from the outputted html, an idea that I borrowed from
+[here]: https://mszep.github.io/pandoc_resume/ .
+
+Simply modify *Resume.elm* to set up the data for your resume.
+
+To build with elm-static-html:
+
+```
+elm-static-html -f ResElmE.elm -o resume.html
+```
---
+++ Resume.elm
@@ -0,0 +1,36 @@
+module Resume exposing (resume)
+
+import Types exposing (Resume, Position, Education)
+
+resume : Resume
+resume =
+    { name = "John Doe"
+    , title = "Sous Chef"
+    , address = "123 North Street, Somecity, AA"
+    , email = "john.doe@johndoe.org"
+    , phone = "+1 (123) 123 - 4567"
+    , positions =
+        [
+            { title = "Sous Chef"
+            , company = "Greasy Spoon Fish'n'Chips"
+            , timeline = "January 1970 - Present"
+            , description = "Performed typical sous chef duties at a local staple restaurant"
+            , points =
+                [ "Performed prep in the mornings"
+                , "Ordered stock from suppliers"
+                ]
+            }
+        ]
+    , educations =
+        [
+            { school = "Somecity High"
+            , graduation = "Spring 1969"
+            , degree = "Diploma"
+            }
+        ]
+    , skills =
+        [ "Cutting"
+        , "Cleaning"
+        , "Fixing equipment"
+        ]
+    }
